159730c1f4b0ed28d8ef32def1133ca04d471e20
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-21 08:15:22 -0300

    Criação do botão mobile

--- inde.js
+++ inde.js
@@ -1,7 +1,18 @@
 function abrir(){
-  
-    document.getElementById("mobile").style.width="50%"
- }
- function fechar(){
-     document.getElementById("mobile").style.width="0%"
- }
+    document.getElementById("btn-content").style.height = "100%"
+    document.getElementById("btn-content").style.visibility = "visible"
+    document.getElementById("ul-mobile").style.visibility = "visible"
+    document.getElementById("btn-content").style.transition = "all 0.5s"
+    document.getElementById("btn-close").style.visibility = "visible"
+    document.getElementById("container").style.backgroundColor = "rgba(0,0,0,0.8)"
+    
+}
+function fechar(){
+document.getElementById("btn-content").style.height = "0%"
+document.getElementById("btn-content").style.visibility = "hidden"
+document.getElementById("ul-mobile").style.visibility = "hidden"
+document.getElementById("btn-content").style.transition = "all 0.5s"
+document.getElementById("btn-close").style.visibility = "hidden"
+document.getElementById("container").style.backgroundColor = "rgba(0,0,0,0)"
+
+}
